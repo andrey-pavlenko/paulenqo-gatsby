b2074b8aeef4b781568624f198608acc95b67308
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2020-10-11 10:31:47 +0300

    Add gallery and image for each product

--- src/products/knot/knot.js
+++ src/products/knot/knot.js
@@ -2,6 +2,8 @@ export const frontmatter = {
   order: 20,
   name: 'Knot',
   price: 79,
+  image: './knot-05.jpg',
+  gallery: './gallery',
   description: 'A denim tote bag with an adjustable handle',
   detailsShipping: ['100% cotton', 'a wide armhole']
 };
--- src/products/soloma/soloma.js
+++ src/products/soloma/soloma.js
@@ -2,6 +2,8 @@ export const frontmatter = {
   order: 50,
   name: 'Soloma',
   price: 159,
+  image: './soloma-05.jpg',
+  gallery: './gallery',
   description: 'A burlap tote bag with hand-burnished leather handles',
   detailsShipping: [
     '100% jute',
